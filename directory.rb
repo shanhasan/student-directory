705f7e8b7a6b9dbf36a724791148ab339e9f90d1
Shan Hasan <shan119@hotmail.co.uk>
2014-10-24 14:07:15 +0100

    Updated code, added methods

--- directory.rb
+++ directory.rb
@@ -11,11 +11,22 @@ students = [
 "Jack"
 ]
 #Print the list of students
+def print_header
 puts "The students of my cohort at Makers Academy"
 puts "_ _ _ _ _ _ _ _ _  _ _"
+end
+
+def print(names)
 students.each do |student|
 	puts student
 end
 
 #finally, we print the total
+def print_footer(names)
 puts "Overall, we have {student.length} great students"
+end
+
+#nothing happens till we call the methods
+print_header
+print(students)
+print_footer(students)
