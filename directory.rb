66a5ad6c0472a3126bbbf8204205888c17d94ce9
Shan Hasan <shan119@hotmail.co.uk>
2014-10-24 17:42:38 +0100

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -36,8 +36,35 @@ def input_students
 end
 
 
-#nothing happens till we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+
+def interactive_menu
+	loop do
+	# 1. print the menu and ask the user what to do
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+	# 2. read the input and save it into a variable
+	selection = gets.chomp
+	# 3. do what the user has asked
+	case selection
+	when "1"
+		# input the students
+		students = input_students
+	when "2"
+		# show the students
+		print_header
+		print (students)
+		print print_footer(students)
+	when "9"
+		exit # this will cause the program to terminate
+	else
+		puts "I don't know what you meant, try again"
+	end
+	# 4. repeat from step 1
+    end
+
+	end
+end
+
+interactive_menu
