93418c82596a873a658843d9f99fa8552f64870d
Shan Hasan <shan119@hotmail.co.uk>
2014-10-24 13:26:53 +0100

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of my cohort at Makers Acadmemy"
 puts "_ _ _ _ _ _ _ _ _ _ _ _ _ _"
 puts "Shan Hasan"
@@ -9,6 +10,8 @@ puts "Bob"
 puts "Jonty"
 puts "George"
 puts "Jack
+#finally, we print the total
 print "Overall, we have "
+# it's important that print() doesn't add new line characters
 print 9
 puts " great students"
